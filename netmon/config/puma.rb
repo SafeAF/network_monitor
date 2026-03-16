0b3f599583af18812217640e7f61eec86b1ccfcb
SafeAF <transiencymail@gmail.com>
2026-03-16 10:49:41 -0600

    upped workers and threads in puma

--- netmon/config/puma.rb
+++ netmon/config/puma.rb
@@ -24,8 +24,14 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 5).to_i
 threads threads_count, threads_count
+
+workers_count = ENV.fetch("WEB_CONCURRENCY", 2).to_i
+if ENV.fetch("RAILS_ENV", "development") == "production" && workers_count > 1
+  workers workers_count
+  preload_app!
+end
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
